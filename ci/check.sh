0be37e2ba7039af78ab5ebf80b1ce0a91703c1e9
AJ Ibraheem <ibraheemaj@icloud.com>
2020-10-28 11:44:04 +0000

    Fix script

--- ci/check.sh
+++ ci/check.sh
@@ -8,4 +8,4 @@ sudo apt-get -y install clang
 # workaround to make clang_sys crate detect installed libclang
 sudo ln -s libclang.so.1 /usr/lib/llvm-10/lib/libclang.so
 
-cargo run --release -p opencv-binding-generator --bin settings-cleanup -- headers src_cpp
+#cargo run --release -p opencv-binding-generator --bin settings-cleanup -- headers src_cpp
